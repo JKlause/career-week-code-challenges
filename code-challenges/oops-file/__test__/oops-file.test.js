6556a66e71b660f2af9191186594f880c864c615
Joe Klause <joeklause@gmail.com>
2019-10-24 16:09:12 -0700

    write function working

--- code-challenges/oops-file/__test__/oops-file.test.js
+++ code-challenges/oops-file/__test__/oops-file.test.js
@@ -1,7 +1,7 @@
 const readDir = require('../helper-functions/read-dir');
 const getDateMod = require('../helper-functions/time-modified')
 const readFile = require('../helper-functions/read-file');
-
+const writeFile = require('../helper-functions/write-file');
 
 describe('oops-file', () => {
 
@@ -18,6 +18,12 @@ describe('oops-file', () => {
 
   it('reads files', () => {
     const text = readFile('code-challenges/oops-file/data/test.txt');
+    expect(text).toBeDefined();
+  });
+
+  it('writes files', () => {
+    writeFile('code-challenges/oops-file/newData/test.txt');
+    const text = readFile('code-challenges/oops-file/newData/test.txt');
     expect(text).toBeDefined();
   });
 
