c9c223d55b6a9594e16535e57eb63f99de1a6f34
Joe Klause <joeklause@gmail.com>
2019-10-24 16:42:20 -0700

    refactored to look clearner (i think)

--- code-challenges/oops-file/oops-file.js
+++ code-challenges/oops-file/oops-file.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 module.exports = (path) => {
   return fs.readdirSync(path)
     .map(file => {
+      
+      const fileContent = fs.readFileSync(`${path}/${file}`, { encoding: 'utf8' });
       const fileName = file.match(/^[^.]*/)[0];
-      return fs.writeFileSync(`${path}-new/${fs.readFileSync(`${path}/${file}`, { encoding: 'utf8' })}-${`${fileName}`}-${fs.statSync(`${path}/${file}`).mtime.toISOString()}`);
+      const dateMod = fs.statSync(`${path}/${file}`).mtime.toISOString()
+      
+      return fs.writeFileSync(`${path}-new/${fileContent}-${`${fileName}`}-${dateMod}`);
     });
 };
